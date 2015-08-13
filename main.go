07b307b7fdb75cfac6d0a3d5f4232f3b278a1029
ggobbe <guillaume.gobbe@gmail.com>
2015-08-13 22:00:51 +0100

    Add type flag to filter on activity type

--- main.go
+++ main.go
@@ -4,6 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"sendgrid-activities/api"
+	"strings"
+)
+
+const (
+	all           = "all"
+	bounces       = "bounces"
+	blocks        = "blocks"
+	invalidEmails = "invalidEmails"
 )
 
 func main() {
@@ -11,43 +19,75 @@ func main() {
 
 	apiUser := flag.String("username", required, "Username to connect to the SendGrid API")
 	apiKey := flag.String("password", required, "Password to connect to the SendGrid API")
+	activityType := flag.String("type", "all", "Types of activities to retrieve (all, bounces, blocks, invalidEmails)")
 	flag.Parse()
 
 	if *apiUser == required || *apiKey == required {
-		fmt.Println("Username and password are required.")
+		fmt.Println("Username and password are required")
 		return
 	}
 
 	sg := api.NewSendGridClient(*apiUser, *apiKey)
 
-	bounces, err := sg.GetBounces()
-	if err != nil {
-		fmt.Println("The response of the Bounces API doesn't match the expected format.\nThis can happen if you have entered the wrong credentials.")
-	} else {
-		printActivities(bounces, "bounce")
+	switch strings.ToLower(*activityType) {
+	case all:
+		printAll(sg)
+	case bounces:
+		activities, err := sg.GetBounces()
+		printActivities(activities, err)
+	case blocks:
+		activities, err := sg.GetBlocks()
+		printActivities(activities, err)
+	case strings.ToLower(invalidEmails):
+		activities, err := sg.GetInvalidEmails()
+		printActivities(activities, err)
+	default:
+		fmt.Printf("Invalid activity type: %s\n", *activityType)
 	}
 
-	blocks, err := sg.GetBlocks()
-	if err != nil {
-		fmt.Println("The response of the Blocks API doesn't match the expected format.\nThis can happen if you have entered the wrong credentials.")
-	} else {
-		printActivities(blocks, "blocks")
-	}
-
-	invalidEmails, err := sg.GetInvalidEmails()
-	if err != nil {
-		fmt.Println("The response of the InvalidEmails API doesn't match the expected format.\nThis can happen if you have entered the wrong credentials.")
-	} else {
-		printActivities(invalidEmails, "invalid-emails")
-	}
 }
 
-func printActivities(activities []*api.Activity, activityType string) {
-	if len(activities) == 0 {
-		return
+func printAll(sg *api.SGClient) {
+	var activities []*api.Activity
+	bouncesActivities, err := sg.GetBounces()
+	if err == nil {
+		activities = append(activities, bouncesActivities...)
+	} else {
+		printError(err)
+	}
+
+	blocksActivities, err := sg.GetBlocks()
+	if err == nil {
+		activities = append(activities, blocksActivities...)
+	} else {
+		printError(err)
+	}
+
+	invalidEmailsActivities, err := sg.GetInvalidEmails()
+	if err == nil {
+		activities = append(activities, invalidEmailsActivities...)
+	} else {
+		printError(err)
+	}
+
+	printActivities(activities, err)
+}
+
+func printActivities(activities []*api.Activity, err error) {
+	if err != nil {
+		printError(err)
+	}
+
+	if len(activities) > 0 {
+		fmt.Printf("\"Created\",\"Status\",\"Email\",\"Reason\"\n")
 	}
 
 	for _, activity := range activities {
-		fmt.Printf("\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n", activityType, activity.Created, activity.Status, activity.Email, activity.Reason)
+		fmt.Printf("\"%s\",\"%s\",\"%s\",\"%s\"\n", activity.Created, activity.Status, activity.Email, activity.Reason)
 	}
+}
+
+func printError(err error) {
+	fmt.Printf("Error whilst retrieving activities: %s\n", err)
+	fmt.Printf("This usually happens if you have entered the wrong credentials.\n")
 }
